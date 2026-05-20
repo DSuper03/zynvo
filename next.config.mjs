80d46296a4b8027d5901bbd80e5bb7b97f72226b
kekubhai <anirbanghosh060@gmail.com>
2026-05-20 20:32:44 +0530

    Remove redirect from www.zynvosocial.com to zynvosocial.com in configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -247,12 +247,6 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.zynvosocial.com' }],
-        destination: 'https://zynvosocial.com/:path*',
-        permanent: true,
-      },
-      {
         source: '/campus-social-media',
         destination: '/',
         permanent: true,
